b84b1bd46ec1b4e4fc09156f7d8bbb22e3e52bc3
Justin Schneck <jschneck@mac.com>
2017-01-17 22:28:42 -0500

    incude test/support when building test env

--- mix.exs
+++ mix.exs
@@ -7,6 +7,7 @@ defmodule Bootloader.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     elixirc_paths: elixirc_paths(Mix.env),
      deps: deps()]
   end
 
@@ -14,6 +15,9 @@ defmodule Bootloader.Mixfile do
     [extra_applications: [],
      mod: {Bootloader, []}]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
     [{:distillery, "~> 1.0", runtime: false}]
