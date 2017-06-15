e0466c5b76fb02aebb809f6e14b4ebccee7e8e47
Justin Schneck <jschneck@mac.com>
2017-06-15 14:59:04 -0400

    update distillery dep

--- mix.exs
+++ mix.exs
@@ -20,7 +20,6 @@ defmodule Bootloader.Mixfile do
   defp elixirc_paths(_),     do: ["lib"]
 
   defp deps do
-    #[{:distillery, "~> 1.0", runtime: false}]
-    [{:distillery, path: "../distillery", runtime: false}]
+    [{:distillery, "~> 1.0", runtime: false}]
   end
 end
