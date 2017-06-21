18a12f019889fa9687f461a93ae82f1e60e804a0
Frank Hunleth <fhunleth@troodon-software.com>
2017-06-21 13:33:22 -0400

    Add changelog and bump version to v0.2.0-dev

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Bootloader.Mixfile do
 
   def project do
     [app: :bootloader,
-     version: "0.1.0",
+     version: "0.2.0-dev",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
