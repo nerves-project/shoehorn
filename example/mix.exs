5ec235aafc5beca23d60d2491c93383f541f3cea
Justin Schneck <jschneck@mac.com>
2018-03-13 21:34:07 -0400

    add example application

---
+++ example/mix.exs
@@ -0,0 +1,28 @@
+defmodule Example.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :example,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      mod: {Example, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:shoehorn, path: "../"}
+    ]
+  end
+end
