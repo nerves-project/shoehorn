ca198daaf0c1afd2b6bf7203ad18b14beb918df1
Justin Schneck <jschneck@mac.com>
2018-08-20 09:15:24 -0400

    v0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Shoehorn.Mixfile do
   def project do
     [
       app: :shoehorn,
-      version: "0.3.1",
+      version: "0.4.0",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
