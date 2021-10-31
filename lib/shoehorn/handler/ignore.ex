64879034c02f05d37ca7bfa08d835898785846c6
Frank Hunleth <fhunleth@troodon-software.com>
2021-10-31 19:39:33 -0400

    Fix warnings from mix docs

--- lib/shoehorn/handler/ignore.ex
+++ lib/shoehorn/handler/ignore.ex
@@ -1,15 +1,20 @@
 defmodule Shoehorn.Handler.Ignore do
-  @moduledoc false
+  @moduledoc """
+  Default handler that ignores all events
+  """
   use Shoehorn.Handler
 
+  @impl Shoehorn.Handler
   def init(_opts) do
     {:ok, :no_state}
   end
 
+  @impl Shoehorn.Handler
   def application_started(_app, state) do
     {:continue, state}
   end
 
+  @impl Shoehorn.Handler
   def application_exited(_app, _reason, state) do
     {:continue, state}
   end
