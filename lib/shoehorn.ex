958607cfdbedc3cd67032d129e314b933ac8238e
Frank Hunleth <fhunleth@troodon-software.com>
2022-03-20 20:09:58 -0400

    Move application callback out of main module

--- lib/shoehorn.ex
+++ lib/shoehorn.ex
@@ -1,31 +1,3 @@
 defmodule Shoehorn do
-  use Application
-
-  @impl Application
-  def start(_type, _args) do
-    opts = [strategy: :one_for_one, name: Shoehorn.Supervisor]
-    Supervisor.start_link(children(), opts)
-  end
-
-  defp children() do
-    case :init.get_argument(:boot) do
-      {:ok, [[bootfile]]} ->
-        bootfile = to_string(bootfile)
-
-        if String.ends_with?(bootfile, "shoehorn") do
-          opts = Application.get_all_env(:shoehorn)
-
-          :error_logger.add_report_handler(Shoehorn.Handler.Proxy, opts)
-
-          [
-            {Shoehorn.ApplicationController, opts}
-          ]
-        else
-          []
-        end
-
-      _ ->
-        []
-    end
-  end
+  @moduledoc false
 end
