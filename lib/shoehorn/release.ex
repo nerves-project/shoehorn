aec11adee9006931c7ecb10e3c47e8c5b008269b
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-03 13:51:13 -0400

    Remove the Utils module

--- lib/shoehorn/release.ex
+++ lib/shoehorn/release.ex
@@ -1,7 +1,19 @@
 defmodule Shoehorn.Release do
   @moduledoc false
 
-  alias Shoehorn.Utils
+  # These applications are unrecoverable using shoehorn
+  @permanent_applications [
+    :shoehorn,
+    :runtime_tools,
+    :kernel,
+    :stdlib,
+    :compiler,
+    :elixir,
+    :iex,
+    :crypto,
+    :logger,
+    :sasl
+  ]
 
   def init(%{boot_scripts: boot_scripts, applications: applications} = release) do
     boot_scripts =
@@ -12,7 +24,7 @@ defmodule Shoehorn.Release do
 
   def start_apps(applications) do
     Enum.filter(applications, fn {name, _opts} ->
-      name in Utils.shoehorn_applications()
+      name in @permanent_applications
     end)
     |> Enum.map(&elem(&1, 0))
     |> Enum.map(&{&1, :permanent})
@@ -20,7 +32,7 @@ defmodule Shoehorn.Release do
 
   def load_apps(applications) do
     Enum.reject(applications, fn {name, _opts} ->
-      name in Utils.shoehorn_applications()
+      name in @permanent_applications
     end)
     |> Enum.map(&elem(&1, 0))
     |> Enum.map(&{&1, :none})
