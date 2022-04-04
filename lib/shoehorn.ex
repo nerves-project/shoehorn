d354b8f78920945f1025d1e1bedff0afadfc33c3
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-04 08:20:14 -0400

    README updates
    This rephrases the purpose of Shoehorn to focus more on the OTP
    application failure handling aspect of it. While Shoehorn does other
    things, like OTP application initialization ordering, this is in support
    of being able to handle unexpected application failures within the VM.
    
    Controlling other aspects of OTP application lifecycle is still done via
    the `Application` module. I'm not aware of any interest in adding to
    Shoehorn to make it do anything more. If anything, Shoehorn has enough
    usage that major changes would need to go elsewhere anyway.

--- lib/shoehorn.ex
+++ lib/shoehorn.ex
@@ -1,5 +1,8 @@
 defmodule Shoehorn do
   @moduledoc """
-  Shoehorn lets you recover from OTP application failures
+  Shoehorn helps you handle OTP application failures
+
+  Shoehorn needs to be enabled when building your project. See the Shoehorn
+  README.md for usage.
   """
 end
