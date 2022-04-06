d5e8a03ac768651fa63c357a042b06215725bd81
Frank Hunleth <fhunleth@troodon-software.com>
2022-04-06 07:45:16 -0400

    Add more tests

--- test/shoehorn/release_test.exs
+++ test/shoehorn/release_test.exs
@@ -2,6 +2,7 @@ defmodule Shoehorn.ReleaseTest do
   use ExUnit.Case, async: true
 
   alias Shoehorn.Release
+  alias Shoehorn.ReleaseError
 
   @example_release %Mix.Release{
     name: :my_project,
@@ -111,7 +112,7 @@ defmodule Shoehorn.ReleaseTest do
     steps: [:assemble]
   }
 
-  test "Shoehorn generates expected app list in order" do
+  test "Generates example app list in order" do
     result = Release.init(@example_release)
     shoehorn_release = result.boot_scripts[:shoehorn]
 
@@ -159,6 +160,88 @@ defmodule Shoehorn.ReleaseTest do
              system_init: :temporary,
              my_project: :temporary,
              iex: :permanent
+           ]
+  end
+
+  test "init apps move earlier" do
+    release = @example_release |> Map.put(:options, shoehorn: [init: [:system_init]])
+    result = Release.init(release)
+    shoehorn_release = result.boot_scripts[:shoehorn]
+
+    # system_init gets started earlier now
+    assert shoehorn_release == [
+             kernel: :permanent,
+             stdlib: :permanent,
+             compiler: :permanent,
+             elixir: :permanent,
+             logger: :permanent,
+             crypto: :permanent,
+             shoehorn: :permanent,
+             sasl: :permanent,
+             system_init: :temporary,
+             crash_app: :temporary,
+             load_only_app: :load,
+             optional_app: :temporary,
+             pure_library: :temporary,
+             some_app: :temporary,
+             my_project: :temporary,
+             iex: :none
+           ]
+  end
+
+  test "init app order is preserved" do
+    release =
+      @example_release |> Map.put(:options, shoehorn: [init: [:system_init, :optional_app]])
+
+    result = Release.init(release)
+    shoehorn_release = result.boot_scripts[:shoehorn]
+
+    # system_init is started right before optional_app and both are at the beginning
+    assert shoehorn_release == [
+             kernel: :permanent,
+             stdlib: :permanent,
+             compiler: :permanent,
+             elixir: :permanent,
+             logger: :permanent,
+             crypto: :permanent,
+             shoehorn: :permanent,
+             sasl: :permanent,
+             system_init: :temporary,
+             optional_app: :temporary,
+             crash_app: :temporary,
+             load_only_app: :load,
+             pure_library: :temporary,
+             some_app: :temporary,
+             my_project: :temporary,
+             iex: :none
+           ]
+  end
+
+  test "last apps go towards the end" do
+    release = @example_release |> Map.put(:options, shoehorn: [last: [:crash_app, :iex]])
+
+    result = Release.init(release)
+    shoehorn_release = result.boot_scripts[:shoehorn]
+
+    # Apps are alphabetically sorted. iex is still last. crash_app is as far back as it can
+    # go since my_project depends on crash_app
+    assert shoehorn_release == [
+             kernel: :permanent,
+             stdlib: :permanent,
+             compiler: :permanent,
+             elixir: :permanent,
+             logger: :permanent,
+             crypto: :permanent,
+             shoehorn: :permanent,
+             sasl: :permanent,
+             load_only_app: :load,
+             optional_app: :temporary,
+             pure_library: :temporary,
+             some_app: :temporary,
+             system_init: :temporary,
+             crash_app: :temporary,
+             my_project: :temporary,
+             iex: :none
            ]
   end
 end
